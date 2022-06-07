c4dbbd6283af323c2aa6cb2e441223016bf63ee0
alejandro111555 <al.mirskoy@gmail.com>
2022-06-07 19:27:59 +0300

    add first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 console.log(galleryItems);
+
+const galleryWrapper = document.querySelector('.gallery');
+
+
+const imgList = galleryItems.map(({preview,description,original})=> `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /> </a> `).join('');
+  galleryWrapper.insertAdjacentHTML('beforeend',imgList);
+
+  galleryWrapper.addEventListener('click' , bigImgGallery);
+
+  let gallery = new SimpleLightbox('.gallery a',{
+    captionsData:`alt`,
+    captionDelay:250 
+  });
+
+  function bigImgGallery (event) {
+    if (event.target.nodeName !== "IMG") {
+        return;
+      }
+}
