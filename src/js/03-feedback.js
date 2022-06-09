9b6dfc2f070463335fafa47bcd33ced9f8dd915d
alejandro111555 <al.mirskoy@gmail.com>
2022-06-09 15:37:32 +0300

    init form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
 
+const formRef = document.querySelector('.feedback-form');
+console.log(formRef);
+const textAreaRef = document.querySelector('.feedback-form textarea');
+console.log(textAreaRef);
+
+formRef.addEventListener('submit', onFormSubmit);
+textAreaRef.addEventListener('input', onTextAreaInput);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+}
+
+function onTextAreaInput(event) {
+    const message = event.currentTarget.value;
+    console.log(message);
+    localStorage.getItem('feedback-form-state',message);
+}
